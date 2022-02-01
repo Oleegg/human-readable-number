ca30a0801c9f338e91a92d2da853988ab809975c
Oleegg <91334112+Oleegg@users.noreply.github.com>
2022-02-01 18:44:32 +0300

    Update index.js
    fix

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-const numberReadable = {1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', 
-6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine', 10: 'Ten', 
-11: 'Eleven', 12: 'Twelve', 13: 'Thirteen', 14: 'Fourteen', 
-15: 'Fifteen', 16: 'Sixteen', 17: 'Seventeen', 18: 'Eighteen', 19: 'Nineteen'}
-numberDozens = ['Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety']
-//module.exports =
-function toReadable (n) {
-    if(n<20) return numberReadable.n
+module.exports = function toReadable(number) {
+    const x = number.toString().split('')
+    if (number == 0) return 'zero'
+    if (number < 20) return `${numberReadable[number]}`
+    if (number < 100) {
+        if (x[1] == 0) return numberDozens[x[0]]
+        return `${numberDozens[x[0]]} ${numberReadable[x[1]]}`
+    }
+    if (number > 99) {
+        const y = +(`${x[1]}${x[2]}`)
+        if (x[1] == 0 && x[2] == 0) return `${numberReadable[x[0]]} ${numberHundred}`
+        if (x[1] == '0') return `${numberReadable[x[0]]} ${numberHundred} ${numberReadable[x[2]]}`
+        if (x[1] == '1') return `${numberReadable[x[0]]} ${numberHundred} ${numberReadable[y]}`
+        if (x[2] == '0') return `${numberReadable[x[0]]} ${numberHundred} ${numberDozens[x[1]]}`
+        return `${numberReadable[x[0]]} ${numberHundred} ${numberDozens[x[1]]} ${numberReadable[x[2]]}`
+    }
 }
+const numberReadable = {
+    1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',
+    6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten',
+    11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen',
+    15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen'
+};
+const numberDozens = { 2: 'twenty', 3: 'thirty', 4: 'forty', 5: 'fifty', 6: 'sixty', 7: 'seventy', 8: 'eighty', 9: 'ninety' };
 
-
-console.log(toReadable(1));  ; // Will return 'one'
-console.log(toReadable(997));  ; //will return 'nine hundred ninety seven'
+const numberHundred = 'hundred';
